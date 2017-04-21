e92f182386f2d1cc039811aca0150a53a889334b
iechenyb <zzuchenyb@sina.com>
2017-04-21 12:26:08 +0800

    add

---
+++ src/test/java/com/genpact/logistics/LogisticsApplicationTests.java
@@ -0,0 +1,26 @@
+package com.genpact.logistics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.genpact.logistics.modules.security.dao.UserRepository;
+import com.genpact.logistics.modules.security.entity.Role;
+import com.genpact.logistics.modules.security.entity.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties="application.properties")
+public class LogisticsApplicationTests {
+	@Autowired
+	private UserRepository userRepository;
+	@Test
+	public void contextLoads() {
+		User user = userRepository.findByUsername("voltest");
+		for(Role role:user.getRoles()){
+			System.out.println(role.getName()+","+role.getType());
+		}
+	}
+
+}
