57f7eeeb8bf813ed07b27355e12e041d96a6ef08
iechenyb <zzuchenyb@sina.com>
2019-05-16 09:54:16 +0800

    commit

--- src/main/java/com/genpact/logistics/modules/security/service/UserService.java
+++ src/main/java/com/genpact/logistics/modules/security/service/UserService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
 import org.springframework.stereotype.Service;
 
 import com.genpact.logistics.modules.security.dao.UserRepository;
@@ -14,7 +13,6 @@ import com.genpact.logistics.modules.security.entity.User;
 
 @Service
 public class UserService implements UserDetailsService {
-	TokenEndpoint x;
 	@Autowired
 	private UserRepository userRepository;
 
